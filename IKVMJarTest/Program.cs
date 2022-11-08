5b986aaeeb0ab190b42c1fc433eb3ce29d2c94ec
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-11-08 18:00:08 +0800

    pdfbox

--- IKVMJarTest/Program.cs
+++ IKVMJarTest/Program.cs
@@ -1,6 +1,11 @@
 ﻿using cn.hutool.core.date;
 using cn.hutool.core.util;
+using cn.hutool.db;
 using com.spire.xls;
+using java.io;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.text;
+using Console = System.Console;
 
 
 var te1 = 123456.123456;
@@ -15,7 +20,22 @@ var format = LocalDateTimeUtil.format(localDateTime, DatePattern.NORM_DATETIME_P
 
 Console.WriteLine(format); //结果:123456.128
 
-com.spire.xls.Workbook spireXlsWorkbook = new com.spire.xls.Workbook();
-spireXlsWorkbook.loadFromFile(@"C:\Users\xkz19\Desktop\WLG-模造IPQC-2H-1664172600044.xlsx");
-Worksheet worksheet = spireXlsWorkbook.getWorksheets().get(0);
-worksheet.saveToImage(@"C:\Users\xkz19\Desktop\WLG-模造IPQC-2H-1664172600044.png");
+// com.spire.xls.Workbook spireXlsWorkbook = new com.spire.xls.Workbook();
+// spireXlsWorkbook.loadFromFile(@"C:\Users\xkz19\Desktop\WLG-模造IPQC-2H-1664172600044.xlsx");
+// Worksheet worksheet = spireXlsWorkbook.getWorksheets().get(0);
+// worksheet.saveToImage(@"C:\Users\xkz19\Desktop\WLG-模造IPQC-2H-1664172600044.png");
+
+PDDocument pdfDoc = PDDocument.load(new java.io.File(@"C:\Users\xkz19\Downloads\asn2.PDF"));
+foreach (PDPage page in pdfDoc.getPages())
+{
+    double pointWidth = page.getMediaBox().getWidth();
+    double pointHeight = page.getMediaBox().getHeight();
+    if (pointHeight > pointWidth)
+    {
+        page.setRotation(90);
+    }
+}
+
+PDFTextStripper stripper = new PDFTextStripper();
+
+Console.WriteLine(stripper.getText(pdfDoc));
